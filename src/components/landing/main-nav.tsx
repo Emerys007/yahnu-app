458738ebde6a6dbdbe90a72a15497b95567095f9
Joel K <joelkatakoanansambi@gmail.com>
2025-07-18 06:51:06 +0000

    Please apply dynamic and engaging animations to all primary call-to-acti

--- src/components/landing/main-nav.tsx
+++ src/components/landing/main-nav.tsx
@@ -147,6 +147,59 @@ export function MainNav() {
             </div>
             
             <div className="md:hidden flex items-center">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="ghost" size="icon">
+                    <MoreVertical className="h-5 w-5" />
+                    <span className="sr-only">More options</span>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuSub>
+                        <DropdownMenuSubTrigger>
+                            <Flag countryCode={country.code} className="mr-2 h-4 w-4" />
+                            <span>{country.name.en}</span>
+                        </DropdownMenuSubTrigger>
+                        <DropdownMenuPortal>
+                            <DropdownMenuSubContent className="w-40 max-h-80 overflow-y-auto">
+                                {allCountries.map((c) => (
+                                    <DropdownMenuItem key={c.code} onClick={() => setCountry(c)}>
+                                        <Flag countryCode={c.code} className="h-4 w-4 mr-2" />
+                                        <span>{c.name.en}</span>
+                                    </DropdownMenuItem>
+                                ))}
+                            </DropdownMenuSubContent>
+                        </DropdownMenuPortal>
+                    </DropdownMenuSub>
+                    <DropdownMenuSub>
+                      <DropdownMenuSubTrigger>
+                        <Languages className="mr-2 h-4 w-4" />
+                        <span>{t('Language')}</span>
+                      </DropdownMenuSubTrigger>
+                      <DropdownMenuPortal>
+                        <DropdownMenuSubContent>
+                          <DropdownMenuItem onClick={() => setLanguage('en')}>English</DropdownMenuItem>
+                          <DropdownMenuItem onClick={() => setLanguage('fr')}>Français</DropdownMenuItem>
+                        </DropdownMenuSubContent>
+                      </DropdownMenuPortal>
+                    </DropdownMenuSub>
+                    <DropdownMenuSub>
+                      <DropdownMenuSubTrigger>
+                        <Sun className="mr-2 h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                        <Moon className="absolute mr-2 h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                        <span>{t('Theme')}</span>
+                      </DropdownMenuSubTrigger>
+                      <DropdownMenuPortal>
+                        <DropdownMenuSubContent>
+                          <DropdownMenuItem onClick={() => setTheme("light")}>{t('Light')}</DropdownMenuItem>
+                          <DropdownMenuItem onClick={() => setTheme("dark")}>{t('Dark')}</DropdownMenuItem>
+                          <DropdownMenuItem onClick={() => setTheme("system")}>{t('System')}</DropdownMenuItem>
+                        </DropdownMenuSubContent>
+                      </DropdownMenuPortal>
+                    </DropdownMenuSub>
+                </DropdownMenuContent>
+              </DropdownMenu>
+
               <Sheet>
                 <SheetTrigger asChild>
                   <Button variant="outline" size="icon">
@@ -203,60 +256,6 @@ export function MainNav() {
                   </div>
                 </SheetContent>
               </Sheet>
-              
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="ghost" size="icon">
-                    <MoreVertical className="h-5 w-5" />
-                    <span className="sr-only">More options</span>
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                    <DropdownMenuSub>
-                        <DropdownMenuSubTrigger>
-                            <Flag countryCode={country.code} className="mr-2 h-4 w-4" />
-                            <span>{country.name.en}</span>
-                        </DropdownMenuSubTrigger>
-                        <DropdownMenuPortal>
-                            <DropdownMenuSubContent className="w-40 max-h-80 overflow-y-auto">
-                                {allCountries.map((c) => (
-                                    <DropdownMenuItem key={c.code} onClick={() => setCountry(c)}>
-                                        <Flag countryCode={c.code} className="h-4 w-4 mr-2" />
-                                        <span>{c.name.en}</span>
-                                    </DropdownMenuItem>
-                                ))}
-                            </DropdownMenuSubContent>
-                        </DropdownMenuPortal>
-                    </DropdownMenuSub>
-                    <DropdownMenuSub>
-                      <DropdownMenuSubTrigger>
-                        <Languages className="mr-2 h-4 w-4" />
-                        <span>{t('Language')}</span>
-                      </DropdownMenuSubTrigger>
-                      <DropdownMenuPortal>
-                        <DropdownMenuSubContent>
-                          <DropdownMenuItem onClick={() => setLanguage('en')}>English</DropdownMenuItem>
-                          <DropdownMenuItem onClick={() => setLanguage('fr')}>Français</DropdownMenuItem>
-                        </DropdownMenuSubContent>
-                      </DropdownMenuPortal>
-                    </DropdownMenuSub>
-                    <DropdownMenuSub>
-                      <DropdownMenuSubTrigger>
-                        <Sun className="mr-2 h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                        <Moon className="absolute mr-2 h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                        <span>{t('Theme')}</span>
-                      </DropdownMenuSubTrigger>
-                      <DropdownMenuPortal>
-                        <DropdownMenuSubContent>
-                          <DropdownMenuItem onClick={() => setTheme("light")}>{t('Light')}</DropdownMenuItem>
-                          <DropdownMenuItem onClick={() => setTheme("dark")}>{t('Dark')}</DropdownMenuItem>
-                          <DropdownMenuItem onClick={() => setTheme("system")}>{t('System')}</DropdownMenuItem>
-                        </DropdownMenuSubContent>
-                      </DropdownMenuPortal>
-                    </DropdownMenuSub>
-                </DropdownMenuContent>
-              </DropdownMenu>
-
             </div>
         </div>
       </div>
