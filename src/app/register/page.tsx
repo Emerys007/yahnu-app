829b8388150536bab9f1b35cc9de370b07c5ca75
Joel K <joelkatakoanansambi@gmail.com>
2025-07-19 04:24:14 +0000

    starting build "ca6e3e11-66ea-4c74-917e-53912a0061ec" FETCHSOURCE hint:

--- src/app/register/page.tsx
+++
@@ -1,6 +0,0 @@
-
-// This page is obsolete and has been replaced by a redirect in /register/route.ts
-// The content has been removed to prevent build errors.
-export default function RegisterPage() {
-    return null;
-}
